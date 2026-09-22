60e1db4c31e225cce545a781b521c55e9a83d59f
vdskevin009 <kevin.vanderstraeten@gmail.com>
2026-09-21 20:50:18 -0700

    Avoid stale PWA shell after deploys

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,12 +1,16 @@
-const CACHE = "familyhub-v2-shell-2";
+const CACHE = "familyhub-v2-shell-3";
 const SHELL = ["./", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(SHELL))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await Promise.all(SHELL.map(async path => {
+      const response = await fetch(path, { cache: "reload" });
+      if (!response.ok) throw new Error(`Unable to cache ${path}`);
+      await cache.put(path, response);
+    }));
+    await self.skipWaiting();
+  })());
 });
 
 self.addEventListener("activate", event => {
@@ -24,7 +28,7 @@ self.addEventListener("fetch", event => {
 
   if (event.request.mode === "navigate") {
     event.respondWith(
-      fetch(event.request)
+      fetch(event.request, { cache: "no-store" })
         .then(response => {
           if (response.ok) caches.open(CACHE).then(cache => cache.put("./", response.clone()));
           return response;
